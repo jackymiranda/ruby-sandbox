51305474e095effe06f45f10c1d25b9ad49aeb31
jackymiranda <jackelyn.miranda@chicagobooth.edu>
2024-10-16 22:39:39 +0000

    hola

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygem.org"
+
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
+
---
+++ hello.rb
@@ -0,0 +1,11 @@
+
+require "active_support/all"
+
+pp "Hi, whats your name"
+
+name = gets.chomp
+
+message =  "Good morning, "+name+ "!"
+
+pp message
+puts message
